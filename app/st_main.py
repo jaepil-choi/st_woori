1213ed190e38676c2fc3257dd81a1b24d0df43ec
jaepil-choi <chljeffreyz@gmail.com>
2022-07-19 03:57:25 +0900

    [FEAT] : Show krw & usd asset values w.r.t fx change

--- app/st_main.py
+++ app/st_main.py
@@ -6,8 +6,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import datetime
+from app.utils import DateUtil
+
 ## custom libs
-import st_utils
+import st_utils, utils
 
 plt.rc('font', family='Malgun Gothic')
 
@@ -25,5 +28,59 @@ APPS = [
 with st.sidebar:
     dropbox = st.selectbox('Select app', APPS)
 
+today = datetime.date.today()
+today_int = DateUtil.timestamp_2_intDate(today)
+today_str = DateUtil.numdate2stddate(today_int)
+
 if dropbox == APPS[0]:
-    pass
+    st.header(APPS[0])
+    st.info('''
+    - 고객이 가진 (달러)외화자산의 가치가 원달러 환율 변동에 의해 얼마나 변하는지 보여줍니다.
+    - 고객 전체 자산에서 원화자산 대비 외화자산이 많을 수록 환율변동에 자산가치가 크게 노출됩니다.
+    - 환율이 10원 변할 때 고객의 전체 자산가치가 몇%나 영향을 받는지 보여줍니다. 
+    - 이를 통해 고객이 환율변동에 너무 큰 영향을 받지 않게 원화자산/외화자산 비중을 적절히 조절하도록 유도합니다.
+    ''')
+    
+    fx_usdkrw = st_utils.get_fdr_data('USD/KRW', start=today_str, end=today_str)
+    fx_usdkrw = utils.get_fdr_last(fx_usdkrw)
+
+    st.subheader(f':dollar: 오늘의 환율: {fx_usdkrw} 원/달러')
+
+    samsung_price = st_utils.get_fdr_data('005930', start=today_str, end=today_str)
+    samsung_price = utils.get_fdr_last(samsung_price)
+    st.write('원화자산을 입력해보세요 (입력 후 엔터)')
+    st.write(f'현재 삼성전자 가격: {samsung_price} 원')
+    samsung_vol = float(st.text_input('삼성전자 몇 주?', 1))
+    
+
+    apple_price = st_utils.get_fdr_data('AAPL', start=today_str, end=today_str)
+    apple_price = utils.get_fdr_last(apple_price)
+    st.write('외화자산을 입력해보세요 (입력 후 엔터)')
+    st.write(f'현재 애플 가격: {apple_price} 달러')
+    apple_vol = float(st.text_input('애플 몇 주?', 1))
+    
+    krw_asset = samsung_vol * samsung_price
+    usd_asset = apple_vol * apple_price
+    
+    krw_total = krw_asset + usd_asset * fx_usdkrw
+    usd_total = krw_asset / fx_usdkrw + usd_asset
+    st.write(f'''
+    현재 고객이 보유한 원화자산총합: {krw_asset} 원
+    현재 고객이 보유한 외화자산총합: {usd_asset} 달러
+
+    총 자산 (원화기준): {krw_total} 원
+    총 자산 (달러기준): {usd_total} 달러
+    ''')
+
+    fx_change_krw = float(st.text_input('원달러 환율 변동값 (기본 50원)', 50))
+    fx_usdkrw_changed = fx_usdkrw + fx_change_krw
+    
+    krw_total_after = krw_asset + usd_asset * fx_usdkrw_changed
+    usd_total_after = krw_asset / fx_usdkrw_changed + usd_asset
+    st.write(f'''
+    원달러 환율이 {fx_change_krw}원 변한다면 고객님 자산은 이런 영향을 받아요.
+
+    총 자산 (원화기준): {krw_total_after - krw_total} 원, {(krw_total_after - krw_total) / krw_total} % 변동
+    총 자산 (달러기준): {usd_total_after - usd_total} 달러, {(usd_total_after - usd_total) / usd_total} % 변동
+    ''')
+    
