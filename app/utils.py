60cf3a39cbda41606117bbe934e0647063953096
jaepil-choi <chljeffreyz@gmail.com>
2022-07-19 02:55:06 +0900

    [FEAT] : Add funcs for stock value/name

---
+++ app/utils.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import pickle
+
+## Custom libs
+from conf import PathConfig
+
+with open(PathConfig.DATA_PATH / 'sid2name.pkl') as p:
+    SID2NAME = pickle.load(p)
+
+def get_FDR_last(df, col='Close'):
+    assert col in ['Close', 'Open', 'High', 'Low', 'Change']
+    
+    row = df.iloc[-1]
+    
+    return row[col]
+
+def sid2name(sid, none_if_not_found=True):
+    assert isinstance(sid, str)
+    
+    try:
+        return SID2NAME[sid]
+    except KeyError as e:
+        if none_if_not_found:
+            return None
+        else:
+            raise e
