45c576ba6317c23f2a1b806b12a26a3d35ef7d83
jaepil-choi <chljeffreyz@gmail.com>
2022-07-21 04:13:16 +0900

    [STYLE] : Fix minor style

--- app/st_main.py
+++ app/st_main.py
@@ -47,7 +47,7 @@ if dropbox == APPS[0]:
     fx_usdkrw = st_utils.get_fdr_data('USD/KRW', start=offset_day_str, end=today_str)
     fx_usdkrw = utils.get_fdr_last(fx_usdkrw)
 
-    st.subheader(f':dollar: 오늘의 환율: {fx_usdkrw} 원/달러')
+    st.subheader(f':dollar: 오늘의 환율: {fx_usdkrw:,} 원/달러')
 
     samsung_logo = 'https://cdn.iconscout.com/icon/free/png-256/samsung-226432.png'
     st.image(samsung_logo, width=100)
@@ -128,7 +128,7 @@ if dropbox == APPS[0]:
         st.write(f'''
         원달러 환율이 {fx_change_krw}원 변한다면 고객님 자산은 이런 영향을 받아요.
 
-        총 자산 (원화기준):
+        변동액 (원화기준):
         ''')
         st.subheader(f'{round(krw_total_after - krw_total, 2):,} 원, {round(100 * (krw_total_after - krw_total) / krw_total, 2)} % 변동')
 
@@ -148,7 +148,7 @@ if dropbox == APPS[0]:
         st.write(f'''
         원달러 환율이 {fx_change_krw}원 변한다면 고객님 자산은 이런 영향을 받아요.
 
-        총 자산 (달러기준):
+        변동액 (달러기준):
         ''')
         st.subheader(f'{round(usd_total_after - usd_total, 2):,} 달러, {round(100 * (usd_total_after - usd_total) / usd_total, 2)} % 변동')
 
